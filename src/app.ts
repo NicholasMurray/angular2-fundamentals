18c57d85866e8bebe64b7df6935512f33fce646b
Nicholas Murray <murray.nicholas@gmail.com>
2016-02-19 22:47:28 +0000

    First 5 lessons

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+//import "./lessons/02-first-lesson/main"
+//import "./lessons/03-events-and-refs/main"
+//import "./lessons/04-events-in-depth/main"
+import "./lessons/05-injecting-a-service/main"
---
+++ src/lessons/03-events-and-refs/todo-input.ts
@@ -0,0 +1,18 @@
+import {Component} from "angular2/core";
+
+@Component ({
+    selector: "todo-input",
+    template: `<div>
+    <input type="text" #myInput />
+    <button (click)="onClick(myInput.value)">Click me</button>
+    </div>`
+})
+
+export class TodoInput{
+    onClick(value) {
+        console.log(value);
+    }
+};
+
+
+
